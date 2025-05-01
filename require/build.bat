0130160f070ac7da525c716d47b1a7e9bb6e0333
unknown <shitubreaker@outlook.com>
2025-05-01 10:53:12 +0800

    残破不堪的初次提交

--- require/build.bat
+++ require/build.bat
@@ -1,5 +1,6 @@
-pip install requests beautifulsoup4 lxml selenium
-pip install pyppeteer
-pip install requests-html
-pip install "lxml[html_clean]"
-pip install lxml_html_clean
+@REM pip install requests beautifulsoup4 lxml selenium
+@REM pip install pyppeteer
+@REM pip install requests-html
+@REM pip install "lxml[html_clean]"
+@REM pip install lxml_html_clean
+@ 本来打算用py实现爬虫 但是后来发现selenium可以直接用node.js运行 就放弃了py
